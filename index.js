a2ebc81428c9f20f2f084ccad14eeba0ce7777f1
Al Shiam Nabil <nabil15-4777@diu.edu.bd>
2025-12-14 02:00:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+app.use(cors());
+app.use(express.json());
+
+require("dotenv").config();
+
+const port = process.env.PORT || 4000;
+
+// mongodb uri connect
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("welcome");
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("clubify");
+    const usersCollection = database.collection("users");
+
+    app.post("/users", async (req, res) => {
+      try {
+        const info = req.body;
+        const query = { email: info.email };
+        const existedEmail = await usersCollection.findOne(query);
+        console.log(existedEmail);
+
+        if (existedEmail) {
+          return res.status(409).json({
+            message: "User already exist.",
+          });
+        }
+
+        info.role = "user";
+        info.createdAt = new Date();
+
+        console.log(info);
+        const result = await usersCollection.insertOne(info);
+        res.json(result);
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({
+          message: "Internal server error.",
+        });
+      }
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`App listening on port : ${port}`);
+});
